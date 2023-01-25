3b554f7f805a66523e50423fc4bedf0dcf46dbba
Douglas Leslie <lesliedouglas23@gmail.com>
2023-01-26 00:28:49 +0100

    save

---
+++ components/coaches/CoachFilter.vue
@@ -0,0 +1,9 @@
+<template>
+  <base-card>
+    <h2>Find Your Coach</h2>
+    <span class="filter-options">
+        <input type="checkbox" id="frontend" checked/>
+        
+    </span>
+  </base-card>
+</template>
