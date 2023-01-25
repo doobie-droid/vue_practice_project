1e135025bde1890791b7f78706d2ec071fe17d90
Douglas Leslie <lesliedouglas23@gmail.com>
2023-01-25 23:17:39 +0100

    still working

--- pages/coaches/CoachesList.vue
+++ pages/coaches/CoachesList.vue
@@ -2,21 +2,50 @@
   <section>FILTER</section>
   <section>
     <div class="controls">
-        <button>Refresh</button>
-        <router-link to="/register">Register as Coach</router-link>
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
     </div>
-    <ul>List of Coaches</ul>
+    <ul v-if="hasCoaches">
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+        >{{ coach.firstName }}</coach-item
+      >
+    </ul>
+    <h3 v-else>No coaches found</h3>
   </section>
-
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 
 export default {
-    computed: {
-        filteredCoaches() {
-        return this.$store.getters['coachModule/coaches']
-    }
- }
-}
+  components: { CoachItem },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coachModule/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coachModule/hasCoaches'];
+    },
+  },
+};
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
