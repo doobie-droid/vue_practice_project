6820723277c36efe08ee4584d5106ec41708990b
Douglas Leslie <lesliedouglas23@gmail.com>
2023-01-26 11:25:44 +0100

    about to cache data locally

--- pages/requests/RequestsReceived.vue
+++ pages/requests/RequestsReceived.vue
@@ -1,10 +1,13 @@
 <template>
+    <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+  <p>{{ error }}</p></base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Requests Received</h2>
       </header>
-      <ul v-if="hasRequests">
+      <base-spinner v-if="isLoading"></base-spinner>
+      <ul v-else-if="hasRequests">
         <request-item
           v-for="req in receivedRequests"
           :key="req.id"
@@ -23,6 +26,12 @@ export default {
   components: {
     RequestItem,
   },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requestModule/requests'];
@@ -30,6 +39,24 @@ export default {
     hasRequests() {
       return this.$store.getters['requestModule/hasRequests'];
     },
+  },
+  created() {
+    this.loadRequests()
+  },
+  methods: {
+    async loadRequests() {
+      //this is async because the thing it is dispatching is async
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requestModule/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Something failed!';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
+    }
   },
 };
 </script>
--- store/requests.js
+++ store/requests.js
@@ -9,6 +9,9 @@ const requestModule = {
     addRequest(state, payload) {
       state.requests.push(payload);
     },
+    setRequests(state, payload) {
+      state.requests = payload;
+    }
   },
   getters: {
     requests(state, getters, rootState, rootGetters) {
@@ -16,20 +19,63 @@ const requestModule = {
       const coachId = rootGetters.userId;
       return state.requests.filter((req) => req.coachId === coachId);
     },
-    hasRequests(state,getters) {
+    hasRequests(state, getters) {
       return getters.requests && getters.requests.length > 0;
     },
   },
   actions: {
-    contactCoach(context, payload) {
+    async contactCoach(context, payload) {
       const newRequest = {
-        id: new Date().toISOString(),
-        coachId: payload.coachId,
+        
         userEmail: payload.email,
         message: payload.message,
       };
+      const response = await fetch(
+        `https://vue-practice-project-34b4f-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
+        {
+          method: 'POST',
+          body: JSON.stringify(newRequest),
+        }
+      );
+      const responseData = await response.json();
+      //firebase returns the ID of that record on the '.name' property
+      newRequest.id = responseData.name;
+      newRequest.coachId = payload.coachId
+
+      if (!response.ok) {
+        const error = new Error(
+          responseData.message || 'Failed to send request'
+        );
+        throw error;
+      }
       context.commit('addRequest', newRequest);
     },
+    async fetchRequests(context) {
+      const coachId = context.rootGetters.userId
+      const response = await fetch(
+        `https://vue-practice-project-34b4f-default-rtdb.firebaseio.com/requests/${coachId}.json`
+      );
+      const responseData = await response.json()
+      if (!response.ok) {
+        const error = new Error(
+          responseData.message || 'Failed to fetsh request'
+        );
+        throw error;
+      }
+
+      const requests = []
+      for (const key in responseData) {
+        const request = {
+          id: key,
+          coachId: coachId,
+          userEmail: responseData[key].userEmail,
+          message:responseData[key].message
+        }
+        requests.push(request)
+      }
+      context.commit('setRequests',requests)
+
+    }
   },
 };
 
